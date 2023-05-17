f796d7b39b59d9754d1104759891218c52ff80e2
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-05-17 18:48:22 +0200

    Indexing and slicing

---
+++ Python101-Beginner/Indexing_and_slicing.py
@@ -0,0 +1,20 @@
+
+lst = ["one","two","three",'four','five']
+print(lst[2])
+print(lst[0:3])
+print(lst[2::])
+print(lst[-1])                  # * without colon it is called as indexing and with colon it is called as slicing 
+print(lst[-1::])
+
+# answer is : three
+# ['one', 'two', 'three']
+# ['three', 'four', 'five']
+# five
+#['five']
+# so this is indexing in array/lists it starts counting item with 0,1,2 and we can print specific item we want
+# apart from lists ,strings are also iterabels
+
+course = "Python 101"
+print(course[5])
+
+# answer is : n
